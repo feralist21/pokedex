20dbaed4b3c070562c214c6c96a900149e9576e7
Alexand Dementev <ad@adictgroup.ru>
2023-10-01 21:31:59 +0800

    Начинает делать стату покемона

---
+++ src/components/molecules/PokemonStats.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="p-4 bg-slate-300 rounded-md flex flex-col gap-y-2">
+		<p class="font-bold text-xl">Stats</p>
+		<div class="flex gap-x-2">
+			<div v-for="(stat, index) in stats" :key="index" class="w-1/6 flex-grow">
+				<div class="flex flex-col gap-y-1">
+					<div v-for="item in constValues.countRow" class="h-1 bg-white rounded-sm"></div>
+				</div>
+				<p class="mt-2 text-xs text-center">{{ stat.stat.name }}</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+import { reactive } from 'vue';
+
+defineProps({
+	stats: {
+		type: Array,
+		default: () => [],
+	},
+});
+
+const constValues = reactive({
+	maxValue: 255,
+	countRow: 15,
+	oneRowValue: 17,
+});
+
+// Methods
+
+// const calcStat = (stat) => {};
+
+// Максимальное значение 255. Есть 15 рядов, 1 ряд = 17 ед. Нужно взять стату покемона и поделить на 17 и округлить в ближайшему целому = получим кол-во рядов, которое надо закрасить
+</script>
--- src/components/pages/DetailPokemon.vue
+++ src/components/pages/DetailPokemon.vue
@@ -15,49 +15,54 @@
 						:alt="pokemonInfo.name"
 					/>
 				</div>
-				<div class="w-full sm:w-1/2 sm:pl-6">
-					<div class="md:w-80 p-4 bg-blue-400 rounded-md">
-						<div class="flex mb-2">
-							<div class="w-1/2 text-white">Base experience</div>
-							<div class="w-1/2 text-black text-right">
-								{{ pokemonInfo.base_experience + ' XP' }}
+				<div class="w-full sm:w-1/2 sm:pl-6 flex gap-x-4">
+					<div class="w-1/2">
+						<div class="md:w-80 p-4 bg-blue-400 rounded-md">
+							<div class="flex mb-2">
+								<div class="w-1/2 text-white">Base experience</div>
+								<div class="w-1/2 text-black text-right">
+									{{ pokemonInfo.base_experience + ' XP' }}
+								</div>
+							</div>
+							<div class="flex mb-2">
+								<div class="w-1/2 text-white">Height</div>
+								<div class="w-1/2 text-black text-right">
+									{{ pokemonInfo.height + ' m' }}
+								</div>
+							</div>
+							<div class="flex">
+								<div class="w-1/2 text-white">Weight</div>
+								<div class="w-1/2 text-black text-right">
+									{{ pokemonInfo.weight + ' kg' }}
+								</div>
 							</div>
 						</div>
-						<div class="flex mb-2">
-							<div class="w-1/2 text-white">Height</div>
-							<div class="w-1/2 text-black text-right">
-								{{ pokemonInfo.height + ' m' }}
-							</div>
+						<div class="mt-4 sm:mt-8">
+							<p class="font-bold text-xl">Abilities</p>
+							<ul class="mt-2 flex gap-4">
+								<pokeTag
+									v-for="(item, index) in pokemonInfo.abilities"
+									:key="index"
+									view="medium"
+									:title="item.ability.name"
+								/>
+							</ul>
 						</div>
-						<div class="flex">
-							<div class="w-1/2 text-white">Weight</div>
-							<div class="w-1/2 text-black text-right">
-								{{ pokemonInfo.weight + ' kg' }}
-							</div>
+						<div class="mt-4 sm:mt-8">
+							<p class="font-bold text-xl">Type</p>
+							<ul class="mt-2 flex gap-4">
+								<pokeTag
+									v-for="(item, index) in pokemonInfo.types"
+									:key="index"
+									view="medium"
+									:title="item.type.name"
+									:bg="item.type.name"
+								/>
+							</ul>
 						</div>
 					</div>
-					<div class="mt-4 sm:mt-8">
-						<p class="font-bold text-xl">Abilities</p>
-						<ul class="mt-2 flex gap-4">
-							<pokeTag
-								v-for="(item, index) in pokemonInfo.abilities"
-								:key="index"
-								view="medium"
-								:title="item.ability.name"
-							/>
-						</ul>
-					</div>
-					<div class="mt-4 sm:mt-8">
-						<p class="font-bold text-xl">Type</p>
-						<ul class="mt-2 flex gap-4">
-							<pokeTag
-								v-for="(item, index) in pokemonInfo.types"
-								:key="index"
-								view="medium"
-								:title="item.type.name"
-								:bg="item.type.name"
-							/>
-						</ul>
+					<div class="w-1/2">
+						<PokemonStats :stats="pokemonInfo.stats"/>
 					</div>
 				</div>
 			</div>
@@ -70,6 +75,7 @@ import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import Container from '@/components/layout/Container.vue';
 import pokeTag from '@/components/atoms/pokeTag.vue';
+import PokemonStats from '@/components/molecules/PokemonStats.vue';
 import PokeAPI from '@/API/PokeAPI';
 
 // data
